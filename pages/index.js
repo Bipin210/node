ee0c64674be2ed6e5940365f0cd17ccd769cd856
Bipin210 <36159299+Bipin210@users.noreply.github.com>
2022-04-06 17:57:54 +0545

    dad

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,6 @@ const { API_URL } = publicRuntimeConfig;
 export default () => (
   <div>
     <h1>Home page (API_URL: {API_URL})</h1>
-
-    <a href="https://github.com/evenchange4/nextjs-pkg-docker-alpine">
-      https://github.com/evenchange4/nextjs-pkg-docker-alpine
-    </a>
+<h1> This is my first CI&CD </h1>
   </div>
 );
