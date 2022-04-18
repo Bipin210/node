38f49fca276de3f81643991a77e6f5eded6247b8
bipin.pandey <bipin@bipin.pandey>
2022-04-18 17:22:41 +0545

    new commit

--- pages/index.js
+++ pages/index.js
@@ -9,5 +9,23 @@ export default () => (
     <h1>Home page (API_URL: {API_URL})</h1>
 <h1> This is my first CI&CD </h1>
 <h1> CICD on kubernetes</h1>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>The href Attribute</h2>
+
+<p>HTML links are defined with the a tag. The link address is specified in the href attribute:</p>
+
+<a href="https://www.w3schools.com">Visit W3Schools</a>
+
+</body>
+</html>
+
+
+
+
   </div>
 );
