542e6d6af1d3e2fa29430c3b3a2c8e5b6559c12b
Bipin210 <36159299+Bipin210@users.noreply.github.com>
2022-12-14 23:19:24 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default () => (
     <h1>Home page (API_URL: {API_URL})</h1>
 <h1> This is my first CI&CD </h1>
 <h1> CICD on kubernetes my cicd does not work properly please help me out</h1>
-<h1> Sagar dai thank you hey baba hey baba </h1>
+<h1> We are in devops training session  </h1>
 <h1> hawa.com.np </h1>
 <h2> Devops Engineer </h2>
   </div>
